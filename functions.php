79c0bf7d89a65626e351d8882769c80efeeb4462
Bryant Cole <bryant.cole@nerdintel.com>
2020-03-04 11:32:46 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 if (!defined('ABSPATH')) die();
 
 //enque divi theme
-function ideakit_ct_enqueue_parent() {
+function churchkit_ct_enqueue_parent() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'churchkit_ct_enqueue_parent' );
